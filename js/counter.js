869c34399ec23d2690dc3b85bd9e2b34c4ffd0fe
Serg-Babak <babakserg1@gmail.com>
2022-12-20 22:46:02 +0200

    add html/css/js counter

---
+++ js/counter.js
@@ -0,0 +1,22 @@
+const incrementBtn = document.querySelector("[data-action='increment']");
+const decrementBtn = document.querySelector("[data-action='decrement']");
+const value = document.querySelector("#value");
+const wraper = document.querySelector("#counter");
+
+let counterValue = 0;
+const increment = () =>{
+    counterValue += 1;
+    value.textContent = counterValue;
+};
+// const decrement = () =>{
+//     counterValue -=1 ;
+//     value.textContent = counterValue;
+// };
+document.querySelector("[data-action='decrement']").onclick = function() {
+  if (counterValue > 1) {
+    value.textContent = --counterValue;
+  } 
+} 
+
+incrementBtn.addEventListener('click', increment);
+decrementBtn.addEventListener('click', decrement);
